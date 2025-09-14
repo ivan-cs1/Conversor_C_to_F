cf4c8494885f2bb648e1e7bfb74c0905c5aa5033
Ivan <ivanfranciscoguerreroreyes@gmail.com>
2025-09-13 21:50:51 -0600

    Initial commit

---
+++ appprueba/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace appprueba
+{
+    public partial class MainPage : ContentPage
+    {
+        float c = 0, f = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClicked(object sender, EventArgs e)
+        {
+            if (float.TryParse(txtcelcius.Text, out float c))
+            {
+                float f = (float)(c * 1.8 + 32);
+
+                lblresultado.Text = $"Resultado: {f} °F";
+            }
+            else
+            {
+                lblresultado.Text = "Por favor ingresa un número válido.";
+            }
+        }
+    }
+
+}
